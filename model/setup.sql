613dfc1cc15d25b42662d5126e0e51f4ae6ec3d3
Adam <8365885+zvakanaka@users.noreply.github.com>
2017-03-02 19:42:23 -0700

    dont judge

--- model/setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'photo'@'localhost' IDENTIFIED BY 'PASSWORD_HERE';
-CREATE DATABASE photo_db;
--- allow some CRUD to happen
-GRANT SELECT, DELETE, INSERT, UPDATE ON photo_db.* TO 'photo'@'localhost';
-FLUSH PRIVILEGES;
