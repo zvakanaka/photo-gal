f51b2283f4be77d72494fcc84ce863732a7ff806
Adam <8365885+zvakanaka@users.noreply.github.com>
2017-04-29 12:14:37 -0600

    neva gonna break my style

--- views/album.php
+++ views/album.php
@@ -2,19 +2,22 @@
 
 <main>
   <h1><?php echo ucfirst($album);?></h1>
-<?php foreach ($images as $image) { ?>
-    <a style="cursor:pointer;" class="thumb-link" id="thumb-<?php echo strip_ext($image).".".$supported_format;?>" href="javascript:void(0)"
-        onClick="getAndShow('<?php echo "$photo_dir/$album/.web/".strip_ext($image).".".$supported_format; ?>',
-                            '<?php echo "$photo_dir/$album/.thumb/".strip_ext($image).".".$supported_format; ?>',
-                            '<?php echo "$photo_dir/$album/$image"; ?>',
-                            '<?php echo $album; ?>')">
-      <!-- TODO: switch big to load from big-thumbs dir or something -->
-      <img class="thumb"
-      big="<?php echo "$photo_dir/$album/.web/".strip_ext($image).".".$supported_format;?>"
-      src="<?php echo "$photo_dir/$album/.thumb/".strip_ext($image).".".$supported_format;?>" alt="<?php echo "Thumbnail of ".$image;?>"/>
-    </a>
-<?php } ?>
-
+  <div class="gal">
+  <?php foreach ($images as $image) { ?>
+    <div>
+      <a style="cursor:pointer;" class="thumb-link" id="thumb-<?php echo strip_ext($image).".".$SUPPORTED_FORMAT;?>" href="javascript:void(0)"
+          onClick="getAndShow('<?php echo "$photo_dir/$album/.web/".strip_ext($image).".".$SUPPORTED_FORMAT; ?>',
+                              '<?php echo "$photo_dir/$album/.thumb/".strip_ext($image).".".$SUPPORTED_FORMAT; ?>',
+                              '<?php echo "$photo_dir/$album/$image"; ?>',
+                              '<?php echo $album; ?>')">
+        <!-- TODO: switch big to load from big-thumbs dir or something -->
+        <img class="thumb"
+        data-fullsize="<?php echo "$photo_dir/$album/.web/".strip_ext($image).".".$SUPPORTED_FORMAT;?>"
+        src="<?php echo "$photo_dir/$album/.thumb/".strip_ext($image).".".$SUPPORTED_FORMAT;?>" alt="<?php echo "Thumbnail of ".$image;?>"/>
+      </a>
+    </div>
+  <?php } ?>
+  </div>
 </main>
 
 <?php if (isset($_SESSION['is_admin'])) {
@@ -44,6 +47,7 @@
 <?php } ?>
 
 <?php include $_SERVER['DOCUMENT_ROOT'].$project_dir.'/views/parts/lightbox.php'; ?>
+
 <!-- if query string photo, open lightbox for it -->
 <?php if ($qs_photo !== NULL) { ?>
   <script type="text/javascript">
--- views/download-upload.php
+++ views/download-upload.php
@@ -1,62 +1,54 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].$project_dir.'/views/parts/head.php'; ?>
 <main>
   <div class="spacer"></div>
-  <div class="center" id="download-album">
-    <h2>Download from Camera</h2>
-    <label for="">Album Name:
+  <h2>Download from DSLR</h2>
+  <div class="center wrap" id="download-album">
+    <label>Album Name:
       <input type="text" class="nice" id="new-album-name" minLength="1" maxlength="32" required/>
       <p class="input-error-message"></p>
     </label>
-      <div class="hidden spacer" id="progress-spacer"></div>
+      <div class="hidden" id="progress-spacer"></div>
       <div class="nice hidden progress-bar" id="download-progress-bar">
         <div class="nice progress-bar-progress" id="download-progress-bar-progress">0%</div>
       </div>
-    <div class="spacer"></div>
     <div>
       <button class="nice" id="download">📷 Download</button>
     </div>
   </div>
 
-  <div class="spacer-big"></div>
-
-  <div class="center" id="upload-to-server">
-    <h2>Upload to Another Server</h2>
-    <label for="">Domain:
-      <input type="text" class="nice" id="domain-name"/>
-    </label>
-    <div class="spacer"></div>
-      <label for="">User:
-      <input title="User name on server" type="text" class="nice" id="user-name"/>
-    </label>
-    <div class="spacer"></div>
-    <label for="">Port:
-      <input title="Server's SSH Port" type="number" class="nice" id="port-number"/>
-    </label>
-    <div class="spacer"></div>
-    <label for="">Album:
-      <select title="Select an existing album to upload" class="nice" id="album-select" name="album_name">
-        <option disabled selected value> - </option>
-        <?php foreach ($albums as $album) { ?>
-            <option value="<?php echo $album;?>">
-              <?php echo $album;?>
-            </option>
-        <?php } ?>
-      </select>
-    </label>
-    <div class="spacer"></div>
-    <div>
-      <button title="Save form to save typing next time" class="nice" id="save-server-details">📄 Save Form</button>
-      <button title="Upload selected album via SCP" class="nice" id="upload">⬆️ Upload</button>
+      <h2>Upload to Another Server</h2>
+  <div class="center wrap" id="upload-to-server">
+      <label for="">Domain:
+        <input type="text" class="nice" id="domain-name"/>
+      </label>
+        <label for="">User:
+        <input title="User name on server" type="text" class="nice" id="user-name"/>
+      </label>
+      <label for="">Port:
+        <input title="Server's SSH Port" type="number" class="nice" id="port-number"/>
+      </label>
+      <label for="">Album:
+        <select title="Select an existing album to upload" class="nice" id="album-select" name="album_name">
+          <option disabled selected value> - </option>
+          <?php foreach ($albums as $album) { ?>
+              <option value="<?php echo $album;?>">
+                <?php echo $album;?>
+              </option>
+          <?php } ?>
+        </select>
+      </label>
+      <div class="spacer"></div>
+      <div>
+        <button title="Save form to save typing next time" class="nice" id="save-server-details">📄 Save Form</button>
+        <button title="Upload selected album via SCP" class="nice" id="upload">⬆️ Upload</button>
+      </div>
+      <div class="nice hidden" id="upload-message">
+        <p></p>
+      </div>
     </div>
-    <div class="nice hidden" id="upload-message">
-      <p></p>
-    </div>
-  </div>
 
-  <div class="spacer-big"></div>
-
+  <h2>Process all Albums</h2>
   <div class="center" id="process-all">
-    <h2>Process all Albums</h2>
     <div>
       <button title="Generate thumbs, webs, and album thumbnails for albums" class="nice" id="process">Process</button>
     </div>
@@ -69,23 +61,4 @@
     updateNumImagesProgress('<?php echo $new_album;?>', '<?php echo $num_images_on_camera;?>');
   </script>
 <?php } ?>
-
-<noscript id="deferred-styles3">
-  <link rel="stylesheet" type="text/css" href="<?php echo $project_dir;?>/styles/photo-gal.css"/>
-</noscript>
-<script>
-  //this is what google pageload recommends so I copied this:
-  var loadDeferredStyles3 = function() {
-    var addStylesNode3 = document.getElementById("deferred-styles3");
-    var replacement = document.createElement("div");
-    replacement.innerHTML = addStylesNode3.textContent;
-    document.body.appendChild(replacement)
-    addStylesNode3.parentElement.removeChild(addStylesNode3);
-  };
-  var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-      webkitRequestAnimationFrame || msRequestAnimationFrame;
-  if (raf) raf(function() { window.setTimeout(loadDeferredStyles3, 0); });
-  else window.addEventListener('load', loadDeferredStyles3);
-</script>
-
 <?php include $_SERVER['DOCUMENT_ROOT'].$project_dir.'/views/parts/toes.php'; ?>
